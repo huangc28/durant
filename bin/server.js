668490af8d161b68a93e8c2b41da5c041c05ca26
Huang Chi-Han <huangchiheng@gmail.com>
2016-12-29 23:50:48 +0800

    Use es2015 on the server side.

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,21 @@
 const fs =  require('fs')
 const path = require('path')
 const config =  JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '.babelrc'), 'utf-8'))
-require('babel-register')(config)
 
+/**
+ * Use "es2015" on the server side.
+ */
+config.presets = config.presets.map(
+  preset => (preset === 'es2015-webpack') ? 'es2015' : preset
+)
+
+require('babel-register')(config)
 require('css-modules-require-hook')({
   generateScopedName: '[name]__[local]___[hash:base64:5]',
 })
 
 require('asset-require-hook')({
-  extensions: ['jpeg', 'jpg', 'svg']
+  extensions: ['jpeg', 'jpg', 'svg'],
 })
 
 require(path.resolve(__dirname, '../server', 'server.js'))
