59e8066a590ae04a9720f22d958b0f4fea7a374b
Huang Chi-Han <huangchiheng@gmail.com>
2017-01-14 23:09:40 +0800

    Change route back.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Route, IndexRoute } from 'react-router'
+import { Route } from 'react-router'
 import App from './Layout/App'
 import Terms from './containers/Terms'
 
 export default (
   <Route path="/" component={App} >
-    <IndexRoute component={Terms} />
+    <Route path="/terms" component={Terms} />
   </Route>
 )
