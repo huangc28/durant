8489565679ad5686361dc6338fe22a61f25bb008
Huang Chi-Han <huangchiheng@gmail.com>
2017-03-12 15:55:38 +0800

    change back to pure function component.

--- src/layout/ForgotPassword/ForgotPassword.jsx
+++ src/layout/ForgotPassword/ForgotPassword.jsx
@@ -1,40 +1,27 @@
-import React, { PropTypes, Component } from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 
 import styles from './ForgotPassword.css'
 import LogoIcon from '../../components/icons/logo'
 
-const OUTSM_OFFICIAL_SITES = 'http://discover.outsm.com'
+const ForgotPassword = ({ children }) => (
+  <div className={styles.root}>
+    {/* logo */}
+    <div className={styles.logo}>
+      <LogoIcon />
+    </div>
 
-class ForgotPassword extends Component {
-  componentWillMount = () => {
-    if (window && window.location && !this.props.token) {
-      window.location.href = OUTSM_OFFICIAL_SITES
-    }
-  }
+    { children }
 
-  render () {
-    return (
-      <div className={styles.root}>
-        {/* logo */}
-        <div className={styles.logo}>
-          <LogoIcon />
-        </div>
-
-        {this.props.children}
-
-        {/* Footer */}
-        <div className={styles.footer}>
-          2016 版權所有為奧仕股份有限公司, OUTSM Inc
-        </div>
-      </div>
-    )
-  }
-}
+    {/* Footer */}
+    <div className={styles.footer}>
+      2016 版權所有為奧仕股份有限公司, OUTSM Inc
+    </div>
+  </div>
+)
 
 ForgotPassword.propTypes = {
   children: PropTypes.node,
-  token: PropTypes.string,
 }
 
 const mapStateToProps = state => ({
